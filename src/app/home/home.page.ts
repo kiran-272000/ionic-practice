c2983915fedf3031777e35ac8c635162102c6753
jeeva <jeevasubash64@gmail.com>
2022-07-29 10:49:18 +0530

    inital

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  categories = [];
+  highlights = [];
+  featured = [];
+
+  catSlideOpts = {
+    slidesPerView: 3.5,
+    freeMode: true,
+    slidesOffsetBefore: 11,
+    spaceBetween: 10
+  };
+  highlightSlideOpts = {
+    slidesPerView: 1.05,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true
+  };
+
+  featuredSlideOpts = {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    freeMode: true
+  };
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json').subscribe((res: any) => {
+      console.log(res);
+
+      this.categories = res.categories;
+      this.highlights = res.highlights;
+      this.featured = res.featured;
+    });
+  }
+
+
+}
